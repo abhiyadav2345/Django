927971b8ca4a8b5a99f1c303e0393bee7b1aaf9e
abhiyadav2345 <abhiyadav2345@gmail.com>
2023-07-16 14:27:08 +0530

    Data types

---
+++ app.py
@@ -0,0 +1,5 @@
+sentence = input("Enter Your Sentence :")
+print("Your name ",+ sentence)
+word1 = input("Enter the word to replace :")
+word2 = input("Enter the word to replace it with :")
+print(sentence.replace(word1, word2))
